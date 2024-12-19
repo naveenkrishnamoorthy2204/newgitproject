67566862b0ddae0b4db7a6f453b4409b111b446c
naveenkrishnamoorthy2204 <naveenkrishnamoorthy20@gmail.com>
2024-12-19 19:23:01 +0530

    all the updaet should commit

--- newproject.java
+++ newproject.java
@@ -3,6 +3,9 @@ package javapractices.bdd;
 public class newproject {
 	public static void main(String[] args) {
 		System.out.println("ho welcome to the git repository");
+		//this is the new commit system 
+
+		
 	}
 
 }
